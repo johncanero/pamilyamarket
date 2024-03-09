039304ff31982ffddb3da92a3258d53783746e62
John Cañero <johncanero.official@gmail.com>
2024-03-09 10:13:47 -0500

    feat: add footer tagline, stay in touch

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,31 @@
+/* eslint-disable react/no-unescaped-entities */
 import Link from "next/link";
 import Image from "next/image";
 
 const Footer = () => {
     return (
-        <div>
-            {/* All rights reserved */}
-            <footer className="my-4 text-sm text-center text-gray-500 md:block">
+        <div className="bg-amber-600">
+            <div className="mx-7 md:mx-24 lg:mx-60 my-8 flex justify-between">
+                <p className="text-xs md:text-xl text-white">Experience Fairview's freshest.</p>
+                <p className="text-xs md:text-xl text-white">Stay in Touch</p>
+            </div>
+
+            <div className="mx-7 md:mx-24 lg:mx-60 mt-4 mb-24 flex justify-between">
+                <p className="text-xl md:text-3xl font-bold tracking-tighter text-white">Follow Pamilya Market</p>
+                {/* Icons */}
+                {/* All rights reserved */}
+            </div>
+            <footer className="my-4 text-sm text-center text-white md:block">
                 <Link
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://www.facebook.com/pamilyamarketph"
                 >
 
-                    <span className=" font-semibold bg-gradient-to-r from-red-400 via-amber-500 to-yellow-500 inline-block text-transparent bg-clip-text mr-1">
-                        Pamilya Market 
+                    <span className=" font-semibold text-white mr-1">
+                        Pamilya Market
                     </span>
-                </Link> 
+                </Link>
                 &copy; {new Date().getFullYear()} All rights reserved.
             </footer>
         </div>
