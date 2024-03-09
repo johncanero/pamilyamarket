154c6cabcbc1cecd8723b8ecd0107a7b2bb2a39b
John Cañero <johncanero.official@gmail.com>
2024-03-08 19:48:30 -0500

    feat: center an image

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <div>
+        <Image
+          src="/images/logo/pamilyaMarketLogo.png"
+          width={250}
+          height={250}
+          alt="pamilya-market-logo"
+          className="mx-auto"
+        />
+    </div>
   );
 }
