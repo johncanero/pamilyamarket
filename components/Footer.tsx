71b81d0160f97ce2e43b7f68be84783e17abf709
John Cañero <johncanero.official@gmail.com>
2024-03-09 09:55:52 -0500

    feat: change text footer color to gradient

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 const Footer = () => {
     return (
         <div>
+            {/* All rights reserved */}
             <footer className="my-4 text-sm text-center text-gray-500 md:block">
                 <Link
                     target="_blank"
@@ -11,7 +12,7 @@ const Footer = () => {
                     href="https://www.facebook.com/pamilyamarketph"
                 >
 
-                    <span className=" font-semibold text-gray-900 dark:text-gray-100 mr-1">
+                    <span className=" font-semibold bg-gradient-to-r from-red-400 via-amber-500 to-yellow-500 inline-block text-transparent bg-clip-text mr-1">
                         Pamilya Market 
                     </span>
                 </Link> 
