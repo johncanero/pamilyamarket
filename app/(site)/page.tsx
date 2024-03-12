2b00c32e6687409c56b910328876c5a34887e55a
John Cañero <johncanero.official@gmail.com>
2024-03-12 12:30:53 -0400

    feat: add framer motion to logo-picture

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,22 @@
+'use client';
+
 import Image from "next/image";
+import { motion } from "framer-motion"
+
+const hover = {
+  variantA: { scale: 1 },
+  variantB: { scale: 1.25 }
+};
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <div >
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        variants={hover} 
+        whileHover={"variantB"}
+      >
         <Image
           src="/images/logo/pamilyaMarketLogo.png"
           width={350}
@@ -11,7 +24,7 @@ export default function Home() {
           alt="pamilya-market-logo"
           className="mx-auto lg:mt-28 mt-44 max-w-xs md:max-w-full"
         />
-      </div>
+      </motion.div>
     </div>
   );
 }
--- components/NavbarMenu.tsx
+++ components/NavbarMenu.tsx
@@ -1,17 +1,26 @@
+'use client';
+
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+
+import { motion } from "framer-motion"
 
 const NavbarMenu = () => {
     return (
         <div className='hidden lg:block'>
             <div className='md:flex md:justify-between lg:mx-16 md:py-6 hidden'>
-                <Image
-                    src="/images/logo/pamilyaMarketText.png"
-                    width={120}
-                    height={120}
-                    alt="pamilya-market-logo-text"
-                />
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                >
+                    <Image
+                        src="/images/logo/pamilyaMarketText.png"
+                        width={120}
+                        height={120}
+                        alt="pamilya-market-logo-text"
+                    />
+                </motion.div>
 
                 <div className='mt-1'>
                     <Link
