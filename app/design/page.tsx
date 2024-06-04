25655c6f44abf6418be630cb04ec7fa1ee8f21e9
John Cañero <johncanero.official@gmail.com>
2024-06-04 12:25:33 -0400

    chore: update alt

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -21,28 +21,28 @@ const page = () => {
                     src="/images/design/exterior0-pamilyaMarket-compressed.png"
                     width={500}
                     height={500}
-                    alt="pamilya-market-location"
+                    alt="pamilya-market-design"
                     className="mx-auto max-w-xs my-10 lg:max-w-full rounded-2xl lg:rounded-3xl"
                 />
                 <Image
                     src="/images/design/exterior1-pamilyaMarket-compressed.png"
                     width={500}
                     height={500}
-                    alt="pamilya-market-location"
+                    alt="pamilya-market-design"
                     className="mx-auto max-w-xs my-10 lg:max-w-full rounded-2xl lg:rounded-3xl"
                 />
                 <Image
                     src="/images/design/exterior2-pamilyaMarket-compressed.png"
                     width={500}
                     height={500}
-                    alt="pamilya-market-location"
+                    alt="pamilya-market-design"
                     className="mx-auto max-w-xs my-10 lg:max-w-full rounded-2xl lg:rounded-3xl"
                 />
                 <Image
                     src="/images/design/exterior3-pamilyaMarket-compressed.png"
                     width={500}
                     height={500}
-                    alt="pamilya-market-location"
+                    alt="pamilya-market-design"
                     className="mx-auto max-w-xs my-10 lg:max-w-full rounded-2xl lg:rounded-3xl"
                 />
             </div>
