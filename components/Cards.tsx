71c8a646c2888330f6d2f160c786326f277452c4
John Cañero <johncanero.official@gmail.com>
2024-06-07 19:53:27 -0400

    chore: change text name button

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -38,7 +38,7 @@ const Cards = () => {
                     <button
                         className="block w-full select-none rounded-b-xl bg-amber-400 py-3.5 px-7 text-center align-middle font-sans text-lg font-bold  text-amber-800 shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         type="button">
-                        Reserve
+                        Browse Products
                     </button>
                 </div>
             </div>
