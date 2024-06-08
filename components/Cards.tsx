ec5e3bca16a8252f90b37308f6d91180aad29cd7
John Cañero <johncanero.official@gmail.com>
2024-06-08 11:48:15 -0400

    feat: add buko stall card

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -176,7 +176,47 @@ const Cards = () => {
                                     <button
                                         className="block w-full select-none rounded-b-xl bg-amber-400 py-3.5 px-7 text-center align-middle font-sans text-lg font-bold  text-amber-800 shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                                         type="button">
-                                        Grocery Store
+                                        Browse Grocery Stall
+                                    </button>
+                                </div>
+                            </div>
+
+
+                        </CarouselItem>
+
+                        {/* Buko Card */}
+                        <CarouselItem
+                            className="md:basis-1/2 lg:basis-1/4"
+                        >
+                            <div className="relative flex w-full lg:max-w-[26rem] max-w-[22rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700">
+                                {/* Card - Buko Contents */}
+                                <div
+                                    className="relative overflow-hidden text-white shadow-lg rounded-t-2xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40 cursor-pointer">
+                                    {/* Card - Buko */}
+                                    <Image
+                                        src="/images/home/card09-buko.png"
+                                        width={1000}
+                                        height={1000}
+                                        alt="buko-card-product" />
+
+                                    {/* Black Gradient */}
+                                    <div
+                                        className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-transparent via-transparent to-black/40">
+                                    </div>
+
+                                    {/* Card - Product */}
+                                    <p
+                                        className="!absolute top-4 left-4 h-8 text-base max-h-[32px] max-w-[32px] select-none rounded-full text-center align-middle font-sans font-semibold text-white transition-all">
+                                        Buko
+                                    </p>
+                                </div>
+
+                                {/* Card - Grocery Button */}
+                                <div>
+                                    <button
+                                        className="block w-full select-none rounded-b-xl bg-amber-400 py-3.5 px-7 text-center align-middle font-sans text-lg font-bold  text-amber-800 shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                        type="button">
+                                        Browse Buko Stall
                                     </button>
                                 </div>
                             </div>
